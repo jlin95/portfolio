9191966eccecb73d894fe7359d55903b9fc944fd
Jialin.Lee <jialin.lee@ninjavan.co>
2018-12-28 00:59:25 +0800

    Add relevant section stubs for page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
+import styled from 'styled-components'
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import { Contact } from '../components/Contact'
+import { Experiences } from '../components/Experiences'
+import { Work } from '../components/Work'
 
-import Layout from '../components/layout'
-import Image from '../components/Image'
-
-const IndexPage = () => (
-  <Layout>
-  <Image/>
-  </Layout>
-
-)
+const IndexPage = () =>
+    <div>Hello world</div>
 
 export default IndexPage
